c73c901297ad5ddf7ee5aa3ab438814ecc976e28
rpardo440828 <70660970+rpardo440828@users.noreply.github.com>
2023-07-07 12:59:59 -0400

    Update schedule.js

--- Webpage/widgets/schedule.js
+++ Webpage/widgets/schedule.js
@@ -1,7 +1,7 @@
 //setInterval(schedule, 1000);
 
 const scheduleIDs = ["schedule_r", "schedule_t", "schedule_c", "schedule_v", "schedule_gl", "schedule_gi"];
-const jsonIDMap = {"schedule_r":"SCHEDULER", 
+const jsonIDMapSchedule = {"schedule_r":"SCHEDULER", 
                     "scheudle_t":"SCHEDULET", 
                     "schedule_c":"SCHEDULEC", 
                     "schedule_v":"SCHEDULEV",
@@ -13,9 +13,30 @@ function schedule(scheduleID, buttonID)
     const date = date => new Date(date.getTime() + new Date().getTimezoneOffset() * -60 * 1000).toISOString().slice(0, 19);
     var str = document.getElementsByClassName(scheduleID).value;
 
-    if(str !== x)
+    if(str !== date)
     {
         schedule(scheduleID, buttonID);
+    }
+    switch(scheduleID)
+    {
+        case 'schedule_r':
+            schedule_r = str;
+            break;
+        case 'schedule_t':
+            schedule_t = str;
+            break;
+        case 'schedule_c':
+            schedule_c = str;
+            break;
+        case 'schedule_v':
+            schedule_v = str;
+            break;
+        case 'schedule_gl':
+            schedule_gl = str;
+            break;
+        case 'schedule_gi':
+            schedule_gi = str;
+            break;
     }
 
     save();
@@ -25,6 +46,25 @@ function schedule(scheduleID, buttonID)
 function setSchedule(scheduleID, storedValue)
 {
     console.log(document.getElementById(scheduleID).value);
-    document.getElementById(scheduleID).value = storedValue;
-
+    switch(scheduleID)
+    {
+        case 'schedule_r':
+            schedule_r = storedValue;
+            break;
+        case 'schedule_t':
+            schedule_t = storedValue;
+            break;
+        case 'schedule_c':
+            schedule_c = storedValue;
+            break;
+        case 'schedule_v':
+            schedule_v = storedValue;
+            break;
+        case 'schedule_gl':
+            schedule_gl = storedValue;
+            break;
+        case 'schedule_gi':
+            schedule_gi = storedValue;
+            break;
+    }
 }
